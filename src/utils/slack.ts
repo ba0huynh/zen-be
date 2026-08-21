11840c29d4c3ebcf0ce7ff9c1b329513261a8f4f
Bao Huynh <124868331+ba0huynh@users.noreply.github.com>
2026-08-21 14:37:15 +0700

    Update slack.ts

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -1,6 +1,6 @@
 import api from "./api";
 
-const URL = "https://hooks.slack.com/services/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"
+const URL = "https://hooks.slack.com/services/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"
 async function sendMessage(text: string) {
     await api.fetch(URL, { body: { text }, method: "POST" });
 }
